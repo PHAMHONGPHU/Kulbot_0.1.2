bfd2f1e3d5b06c6e9cd9ad4a090a1cb82c533e53
PHAMHONGPHU <105095824+PHAMHONGPHU@users.noreply.github.com>
2023-05-13 09:40:07 +0700

    KULBOT.ino

--- examples/testKULBOT.ino
+++ examples/testKULBOT.ino
@@ -1,30 +1,13 @@
-#include "KULBOT.h"
+#include <KULBOT.h>
 KULBOT Rob;
 int servo_position[9];
     float increment[9];
     unsigned long final_time;
     unsigned long partial_time;
     int speed_1;
-void IRAM_ATTR toggleLED()
-{
-//  long unsigned int start;  // Used for timing
-//  long unsigned int now;    // Used for timing
-delay(100);
-digitalWrite(CONTROL_POWER,LOW);
-delay(1000);
-}
 void setup() {
 
 Rob.KULBOT_INIT();
-
-   pinMode(CONTROL_POWER,OUTPUT);
-   digitalWrite(CONTROL_POWER,HIGH);
-   pinMode(INPUT_BUTTON_POWER, INPUT);
-   attachInterrupt(INPUT_BUTTON_POWER, toggleLED,RISING);
-  
-  pinMode(LED_GREEN_POWER,OUTPUT);
-  pinMode(LED_RED_POWER,OUTPUT);
-  digitalWrite(LED_RED_POWER,0);
 //Rob.KULBOT_RGB_INIT(100);
 Rob.KULBOT_SERVO_INIT();
 Rob.KULBOT_SENSOR_INIT();
